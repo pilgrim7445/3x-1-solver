2db4692e7cd721785e29842a1623b4ede77a3d4e
Pilgrim <152723591+pilgrim7445@users.noreply.github.com>
2024-12-04 11:55:05 +0000

    Update 3x+1.py

--- 3x+1.py
+++ 3x+1.py
@@ -5,10 +5,10 @@ n = int(input("Number: "))
 while True:
     if (n % 2) == 0:
        n = n/2
-       print(n)
+       print(int(n))
     else:
        n = n*3+1
-       print(n)
+       print(int(n))
        
     if n == 1:
         break
