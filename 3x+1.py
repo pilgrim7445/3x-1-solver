149f0425dc859ccacc8e5bef1ca8a3e94b89eca1
Pilgrim <152723591+pilgrim7445@users.noreply.github.com>
2025-05-17 10:04:21 +0100

    Update 3x+1.py

--- 3x+1.py
+++ 3x+1.py
@@ -2,16 +2,19 @@ print("3x+1 solver")
 
 n = int(input("Number: "))
 a = n
+iter = 0
 
 while True:
     if (a % 2) == 0:
        a = a/2
-       print(int(n))
+       print(int(a))
+        iter += 1
     else:
        a = a*3+1
-       print(int(n))
-       
+       print(int(a))
+       iter += 1
     if a == 1:
         break
 
-print("The number {a}")
+print("")
+print("The number ", a, "took", iter, "iterations to get to 1.")
