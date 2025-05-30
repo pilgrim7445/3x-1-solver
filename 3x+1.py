8754ac524dd1b597f3589bbb2332997d647971be
Pilgrim <152723591+pilgrim7445@users.noreply.github.com>
2025-05-30 10:20:31 +0100

    Update 3x+1.py

--- 3x+1.py
+++ 3x+1.py
@@ -1,20 +1,24 @@
 print("3x+1 solver")
 
-n = int(input("Number: "))
-a = n
-iter = 0
+mode = int(input("Mode (1 or 2): "))
+if mode == 1:
+    n = int(input("Number: "))
+    a = n
+    iter = 0
 
-while True:
-    if (a % 2) == 0:
-       a = a/2
-       print(int(a))
-        iter += 1
-    else:
-       a = a*3+1
-       print(int(a))
-       iter += 1
-    if a == 1:
-        break
+    while True:
+        if (a % 2) == 0:
+           a = a/2
+           print(int(a))
+            iter += 1
+        else:
+           a = a*3+1
+           print(int(a))
+           iter += 1
+        if a == 1:
+            break
+if mode == 2:
+    print("wait for an upd")
 
 print("")
-print("The number ", a, "took", iter, "iterations to get to 1.")
+print("The number ", n, "took", iter, "iterations to get to 1.")
